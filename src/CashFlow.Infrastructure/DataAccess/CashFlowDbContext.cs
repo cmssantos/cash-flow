dd12f8c41d09804a4214e6878030de7ac68ac3fe
cleudicesantos <cleudice.santos@mega.com.br>
2025-01-30 11:52:16 -0300

    database

---
+++ src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
@@ -0,0 +1,17 @@
+using CashFlow.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CashFlow.Infrastructure.DataAccess;
+
+public class CashFlowDbContext : DbContext
+{
+    public DbSet<Expense> Expenses { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var connectionString = "Server=10.0.0.10;Database=cashflow;Uid=root;Pwd=@#123abc";
+        var serverVersion = new MySqlServerVersion(new Version(major: 8, minor: 4, build: 4));
+
+        optionsBuilder.UseMySql(connectionString, serverVersion);
+    }
+}
